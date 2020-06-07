d6baaaf63c7aa2d092b03a19e1f5521fb0afd2b0
Jonathan Snipe <jon.mike919@gmail.com>
2020-06-07 15:28:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
   if phrase != phrase.upcase
-    "HUH?! SPEAK UP SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase && phrase == 'I LOVE YOU GRANDMA'
     "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO NOT SINCE 19381"
+    "NO NOT SINCE 1938"
   end
 end
 
